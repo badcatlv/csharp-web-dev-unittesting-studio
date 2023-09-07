b1af04e65e81a260a1efe461d66f2801d48bfd96
Colleen Blake <colleenemilyblake@gmail.com>
2023-09-06 19:18:47 -0500

    with 6 test

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -1,4 +1,5 @@
 ﻿namespace BalancedBracketsTests;
+using BalancedBrackets;
 
 [TestClass]
 public class BalancedBracketsTests
@@ -10,4 +11,31 @@ public class BalancedBracketsTests
     {
         Assert.AreEqual(true, true);
     }
+
+    [TestMethod]
+    public void HasBalancedBrackets_WithOnlyBrackets_IsTrue()
+    {
+        Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]"));
+    }
+
+    [TestMethod]
+    public void HasBalancedBrackets_WithDoubleBrackets_IsTrue()
+    {
+        Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[][]"));
+    }
+
+    [TestMethod]
+    public void HasBalancedBrackets_WithNestedBrackets_IsTrue()
+    {
+        Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[[]]"));
+    }
+
+    [TestMethod]
+    public void HasBalancedBrackets_WithNestedDoubleBrackets_IsTrue()
+    {
+        Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[[][]]"));
+    }
+
+    [TestMethod]
+    public void HasBalancedBrackets_BracketsWithCharactersWithInBrackets_IsTrue()
 }
